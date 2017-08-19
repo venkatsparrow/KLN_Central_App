6d205fa162f6e8b6c314c8705958a4592aee97d9
sairamkamalay <godissairam@gmail.com>
2017-08-19 13:22:19 +0530

    sidemenu with logout functionality addded

--- app/src/main/java/edu/klnce/sairam_kamalay/klncecentral/ImageListActivity.java
+++ app/src/main/java/edu/klnce/sairam_kamalay/klncecentral/ImageListActivity.java
@@ -1,6 +1,10 @@
 package edu.klnce.sairam_kamalay.klncecentral;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +12,8 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,7 +23,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ImageListActivity extends AppCompatActivity {
+public class ImageListActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DatabaseReference mDatabaseRef;
     private List<imageUpload> imgList;
     private ListView lv;
@@ -26,8 +32,10 @@ public class ImageListActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private NavigationView mDrawer;
 
 
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +47,8 @@ public class ImageListActivity extends AppCompatActivity {
         progressDialog.setMessage("please wait loading News...");
         progressDialog.show();
 
+        mDrawer = (NavigationView) findViewById(R.id.sidenavview);
+        mDrawer.setNavigationItemSelectedListener(this);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.imagelistDrawer);
         mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.sidenavopen,R.string.sidenavclose);
 
@@ -71,11 +81,20 @@ public class ImageListActivity extends AppCompatActivity {
         });
     }
 
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (mToggle.onOptionsItemSelected(item)){
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Intent tolog = null;
+        if (item.getItemId() == R.id.sidenav_logout){
+            firebaseAuth.signOut();
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+            tolog = new Intent(ImageListActivity.this,MainActivity.class);
+            tolog.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(tolog);
             return true;
         }
-        return super.onOptionsItemSelected(item);
+
+
+        return false;
     }
 }
