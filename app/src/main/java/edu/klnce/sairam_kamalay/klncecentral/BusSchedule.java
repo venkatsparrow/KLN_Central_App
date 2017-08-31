e593c8fb9e42c13d171b34db6489785d211b9056
sairamkamalay <godissairam@gmail.com>
2017-08-31 21:36:34 +0530

    bus schedule added

---
+++ app/src/main/java/edu/klnce/sairam_kamalay/klncecentral/BusSchedule.java
@@ -0,0 +1,13 @@
+package edu.klnce.sairam_kamalay.klncecentral;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BusSchedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_schedule);
+    }
+}
